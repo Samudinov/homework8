5f652e3076cde24e00e554c2b3e7cb1393ae2719
Samudinov <104208404+Samudinov@users.noreply.github.com>
2022-05-20 20:06:47 +0600

    first commit

---
+++ bin/dart_app_func8.dart
@@ -0,0 +1,5 @@
+import 'package:dart_app_func8/dart_app_func8.dart' as dart_app_func8;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_app_func8.calculate()}!');
+}
---
+++ test/dart_app_func8_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_app_func8/dart_app_func8.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
